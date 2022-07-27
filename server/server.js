0e89ea11ea7f6b3c0ab72b09d5272eeec490fd15
Devin Lorenz <devinlorenz.work@gmail.com>
2022-07-27 15:41:25 -0400

    made localhost work

--- server/server.js
+++ server/server.js
@@ -5,10 +5,15 @@ const cors = require('cors');
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 const port = process.env.PORT || 4004;
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+  })
+
 
 app.listen(port, () => {
     console.log(`Server jamming on port ${port}`);
